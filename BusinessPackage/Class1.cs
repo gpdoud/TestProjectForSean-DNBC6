f78ffa7509af12cbae76f10ebc75846ec6d69acf
Greg Doud <gpdoud@gmail.com>
2019-08-16 08:56:52 -0400

    Add project files.

---
+++ BusinessPackage/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BusinessPackage {
+    public class Ball : Base {
+
+        public int Id { get; private set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+
+        public Ball() {
+            this.Id = 0;
+        }
+        public Ball(string color, string size) : this() {
+            this.Color = color;
+            this.Size = size;
+        }
+    }
+    public class Base {
+        public int Code { get; set; }
+    }
+}
---
+++ TestProjectForSean/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessPackage;
+
+namespace TestProjectForSean {
+    class Program {
+        static void Main(string[] args) {
+            var ball = new Ball();
+
+            Console.Write("Enter color: ");
+            var color = Console.ReadLine();
+            Console.Write("Enter size: ");
+            var size = Console.ReadLine();
+            var ball1 = new Ball() { Color = color, Size = size };
+            var ball2 = new Ball("Red", "Lrg");
+            Ball[] balls = new Ball[3];
+            balls[0] = ball;
+
+            List<Ball> balls2 = new List<Ball>();
+            balls2.Add(ball1);
+        }
+    }
+}
